17388c9b555bda612d260acca92946b5c3503450
gitcat456 <okothdenzel65@gmail.com>
2025-09-28 13:22:44 +0300

    Task completed

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            item_name = input("Enter name of whatever you wish to add: ")
+            shopping_list.append(item_name)
+            print(f"'{item_name}' added to your list!")
+            
+        elif choice == '2':
+            remove = input("Enter the name of what you wish to remove: ")
+            
+            if remove in shopping_list:
+                shopping_list.remove(remove)  
+                print(f"'{remove}' removed from your list!")
+            else:
+                print(f"'{remove}' not found in your list.")
+            
+        elif choice == '3':
+             if shopping_list:
+                print("Your Shopping List:")
+                for i, item in enumerate(shopping_list, 1):
+                    print(f"{i}. {item}")
+             else:
+                print("Your shopping list is empty!")
+        
+        elif choice == '4':
+            print('Goodbye')
+            break
+        else: 
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
