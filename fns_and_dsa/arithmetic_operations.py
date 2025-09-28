63f21fdf4e5abcf2c0f041c588e13cc24e674825
gitcat456 <okothdenzel65@gmail.com>
2025-09-28 12:55:04 +0300

    minor fixes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,15 @@
 def perform_operation(num1, num2, operation):
     
-    match operation:
-        case 'add': 
-            result = num1+num2
-            return result
-        case 'subtract':
-            result = num1-num2
-            return result
-        case 'multiply':
-            result = num1*num2
-            return result
-        case 'divide':
-            if num2 == 0:
-             return "Error: Division by zero is not allowed"
-            else:
-             result = num1/num2
-             return result 
-        case _:
-            return 'Invalid Operation'
+      if operation == 'add':
+        return num1 + num2
+      elif operation == 'subtract':
+        return num1 - num2
+      elif operation == 'multiply':
+        return num1 * num2
+      elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        else:
+            return num1 / num2
+      else:
+        return "Error: Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'"
