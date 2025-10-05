90e4b68d6deb103b4824c5f8c08773c2a2509fc7
gitcat456 <okothdenzel65@gmail.com>
2025-10-05 12:14:18 +0300

    test enhancements

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance, initial_balance):
         self.account_balance = account_balance
+        self.initial_balance = 0
         
     def deposit(self, amount):
         self.account_balance += amount
