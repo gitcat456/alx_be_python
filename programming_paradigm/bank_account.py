56ae760357087a00ab1ab19e53bffaf0a75f4f93
gitcat456 <okothdenzel65@gmail.com>
2025-10-05 12:21:09 +0300

    test enhancements

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,17 @@
 class BankAccount:
-    def __init__(self, account_balance, initial_balance):
-        self.account_balance = account_balance
-        self.initial_balance = 0
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
         
     def deposit(self, amount):
         self.account_balance += amount
-        return self.account_balance
-        
+          
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
             return True
         else:
             return False
+        
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
             
