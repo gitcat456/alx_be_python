a4be6ebc1a162bd51e64991a3498c55e5942be4f
gitcat456 <okothdenzel65@gmail.com>
2025-10-14 15:09:52 +0300

    adds a while statement

--- condition_structs/forloop.py
+++ condition_structs/forloop.py
@@ -30,3 +30,9 @@ for key, value in user.items():
 for i in range(3):
      for j in range(2):
          print(f"i={i}, j={j}")
+         
+
+count = 0
+while count<5:
+    print(count)
+    count += 1
