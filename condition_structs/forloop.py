13b7d4e46b7e030ebb5bc3c274dec8ebf32460e7
gitcat456 <okothdenzel65@gmail.com>
2025-10-14 14:38:10 +0300

    practicing for loops

---
+++ condition_structs/forloop.py
@@ -0,0 +1,5 @@
+#syntax  : for variable in iterable:    where iterable is something you can loop over
+           #condition to be executed
+           
+for i in range(10):   #i<10
+    print(f"loop:{i}")
