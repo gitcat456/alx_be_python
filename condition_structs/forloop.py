cd974d5c939eb474c25364bf3db50215e2bdc7de
gitcat456 <okothdenzel65@gmail.com>
2025-10-14 14:43:20 +0300

    adds code to loop over a list of items

--- condition_structs/forloop.py
+++ condition_structs/forloop.py
@@ -1,5 +1,10 @@
 #syntax  : for variable in iterable:    where iterable is something you can loop over
            #condition to be executed
            
-for i in range(10):   #i<10
+for i in range(3):   #i<10
     print(f"loop:{i}")
+    
+#looping over a list 
+languages = ["python","javascript", "java", "mySql", "Cpp"]
+for language in languages:
+    print(f"The user has experience in {language}")
