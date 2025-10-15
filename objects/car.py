c59ddc519aaab06def02ed56222ea7b6f9dd2479
gitcat456 <okothdenzel65@gmail.com>
2025-10-15 20:52:32 +0300

    minor tests

--- objects/car.py
+++ objects/car.py
@@ -11,10 +11,21 @@ class Anime:
     def get_info(self):
         return f"{self.name} from {self.country} rating of {self.rating}"
     
+    def main_character(self, mainxter):
+        self.mainxter = mainxter
+        return f"The main characters name is {self.mainxter}"
+    
+    def get_move(self, move):
+        self.move = move
+        return f"His Killer move is {self.move} "
+        
+    
 
 index1 = Anime("Dragon Ball Super", 9.5, "Japan")
 index2 = Anime("Ice Cube", 8.3, "China")
 index3 = Anime("One Piece", 9.3, "Japan")
+
+print(index1.get_move("Spirit Bomb!"))
 
 objs = [index1,index2,index3]
 for obj in objs:
