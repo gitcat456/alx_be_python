1b138de9b0c28bf91500d78d43ac5b592cbed43c
gitcat456 <okothdenzel65@gmail.com>
2025-10-15 15:41:57 +0300

    works!

--- objects/car.py
+++ objects/car.py
@@ -4,10 +4,20 @@ class Anime:
         self.rating = rating
         self.country = country
         
+     #allows you to print(index1)  directly without calling a function 
+    def __str__(self):
+        return f"{self.name} from {self.country} rating of {self.rating}"
+        
     def get_info(self):
         return f"{self.name} from {self.country} rating of {self.rating}"
     
 
 index1 = Anime("Dragon Ball Super", 9.5, "Japan")
-print(index1)
+index2 = Anime("Ice Cube", 8.3, "China")
+index3 = Anime("One Piece", 9.3, "Japan")
+
+objs = [index1,index2,index3]
+for obj in objs:
+    print(obj)
+
         
