da0a195cf6d9e5e3dde4ab7aeec0d4f1b9ea363a
gitcat456 <okothdenzel65@gmail.com>
2025-10-15 15:51:34 +0300

    creates a car class with sample functions

---
+++ objects/catalo.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0  # Default attribute value
+
+    def get_descriptive_name(self):
+        full_name = f"{self.year} {self.make} {self.model}"
+        return full_name
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+           self.odometer_reading += miles
