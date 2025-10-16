042a8f5f46e2d14e793af58410ddd2f6a10bad3b
gitcat456 <okothdenzel65@gmail.com>
2025-10-16 13:08:49 +0300

    simple code block to define static methods

---
+++ decorators/staticmethod.py
@@ -0,0 +1,12 @@
+class MathUtils:
+    @staticmethod
+    def add(a, b):
+        return a+b
+    
+    @staticmethod
+    def multiply(a,b):
+        return a * b
+    
+#usage
+print(MathUtils.add(5, 4))
+print(MathUtils.multiply(5, 4))
