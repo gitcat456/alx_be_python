8f2037a2a6895519fe7f19d01628c9b06d44e22d
gitcat456 <okothdenzel65@gmail.com>
2025-10-19 22:42:43 +0300

    Creates blueprints for various tables

---
+++ Intro_to_DB/alx_book_store.sql
@@ -0,0 +1,47 @@
+import mysql_connector_python
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="Astax",
+    password="Pt$#523jI",
+    database="alx_book_store"
+)
+
+cursor = mydb.cursor()
+
+table_definitions = [
+
+    """
+    CREATE TABLE IF NOT EXISTS Authors (
+        author_id INT PRIMARY KEY,
+        author_name VARCHAR(215)
+    )
+
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Books (
+        book_id INT PRIMARY KEY,
+        title VARCHAR(130),
+        author_id INT,
+        price DOUBLE,
+        publication_date DATE,
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    )
+
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS Customers (
+        customer_id INT PRIMARY KEY,
+        customer_name VARCHAR(215),
+        address TEXT
+    )
+    """,
+
+    """
+    """,
+
+    """
+    """
+]
