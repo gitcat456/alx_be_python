8f87070691d31604b838450fcb72b80ed13f7281
gitcat456 <okothdenzel65@gmail.com>
2025-10-21 23:48:30 +0300

    examples of for loop use cases

---
+++ condition_structs/loops.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+    
+for num in range(1,6):
+    print(num)
+    
+numbers = [1, 3, 3, 4 ]
+total=0
+
+for numb in numbers:
+    total+=numb
+print(f"your sum is:{total}")
