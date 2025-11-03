5dfdf0a46a649f8db6135f8e3a5cc1b560735a0e
gitcat456 <okothdenzel65@gmail.com>
2025-11-03 22:36:01 +0300

    creates a list of books

---
+++ library_system/books.py
@@ -0,0 +1,18 @@
+books = [ {
+    'name':'Desire of Ages',
+    'author':'Ellen White'
+    },
+    {
+     'name':'Rich Dad Poor Dad',
+     'author':'Jon Doe'
+    },
+    {
+      'name':'The deception',
+      'author': 'O.Denzel'
+    },
+]
+
+def add_book(name, author):
+    books.append({'name':name, 'author':author})
+    print(books)
+    
